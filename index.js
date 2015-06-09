9670b20b47158869311f50bfc98014393f34c3ef
martinacd08 <casandra123>
2015-06-08 23:38:40 -0700

    Revert "1"
    This reverts commit 8df2a724f152f933caa69c2f76317167713834e1.

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 
 var express = require('express'),
     app     = express();
-
+var business = require('./business.js');
+	
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(__dirname + '/cetys'));
 app.use(express.static(__dirname + '/UI'));
 app.use(express.static(__dirname + '/UI/assets'));
 
+
+app.get('/cetys', function(req, res) {
+	res.sendfile(__dirname + '/cetys/portfolio_two.html');
+});
 
 app.get('/index', function(req, res) {
 	res.sendfile(__dirname + '/UI/index.html');
 });
 
+
+app.get('/fracs', business.getAllFracs);
+app.get('/frac/:id', business.getFracByID);
+app.get('/frac/:id/saldoCorriente', business.getSaldoCorrienteByFrac);
+app.get('/frac/:id/saldoVencido', business.getSaldoVencidoByFrac);
+
+app.get('/frac/:id/saldoRecuperado', business.getSaldoRecuperadoByFrac);
+
+app.get('/frac/:id/expedientes', business.getExpedientesByFrac);
 
 
 app.listen(app.get('port'), function() {
